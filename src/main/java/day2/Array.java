ed57a8a62ed94d8a972ccc618d93b31905b969f1
guri670 <kory0485@gmail.com>
2024-08-26 13:31:32 +0900

    :day2

--- src/main/java/day2/Array.java
+++ src/main/java/day2/Array.java
@@ -8,5 +8,40 @@ public class Array {
         int c = 30;
         int d = 40;
 
+        System.out.println(a);
+        System.out.println(b);
+        System.out.println(c);
+        System.out.println(d);
+
+        //변수가 많아지면 변수를 관리하는 것도 힘들고 사용하는 것도 힘들다.
+        //1. 변수명이 많아져서 힘들다. -> 변수명을 한개만 쓰면 됨.
+        //2. 변수가 많아서 다 사용하기 힘듦. -> 반복문을 사용하면 됨.
+
+        // 결론 : 배열을 사용하면 됨.
+
+//        int[] arr = {10, 20, 30, 40, 50}; //배열의 표현 방법
+//        System.out.println(arr[3]);
+//
+//        System.out.println("====================================");
+//        System.out.println(arr[2]);
+//        arr[2] = 100; //배열의 특정 위치의 값을 새로운 값으로 대입 가능.
+//        System.out.println(arr[2]);
+//        System.out.println("====================================");
+//
+//        System.out.println(arr[0]);
+//        System.out.println(arr[1]);
+//        System.out.println(arr[2]);
+//        System.out.println(arr[3]);
+//        System.out.println(arr[4]);
+//        for(int i = 0 ; i <= 4 ; i++){
+//            System.out.println(arr[i]);
+
+
+        int[] arr=  {10,20,30,40,50,60,70,80,90,100};
+        for(int j = 0 ; arr[]; j++){
+            System.out.println(arr[j]);
+        }
+        System.out.println(arr.length);//배열의 길이 정보 확인
+        //내가 저장할 값이 몇개인지 모를 때.
     }
 }
